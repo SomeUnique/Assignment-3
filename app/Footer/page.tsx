6763a34401fac6c5a25e7148e96b5d13e4447c35
SomeUnique <expertbychance@gmail.com>
2024-11-02 00:29:11 +0500

    Add files via upload

---
+++ app/Footer/page.tsx
@@ -0,0 +1,14 @@
+const footer=()=>{
+    return(
+        <footer>
+            <nav>
+                <h1><b>Footer</b></h1>
+                <ol>
+                    <li>total iteams</li>
+                    <li>5</li>
+                </ol>
+            </nav>
+        </footer>
+    )
+}
+export default footer
---
+++ app/header/page.tsx
@@ -0,0 +1,14 @@
+export default function header(){
+    return(
+        <div>
+            <h1><b>Header</b></h1>
+            <ol>
+                <li>CDs</li>
+                <li>Ear-pods</li>
+                <li>Charger</li>
+                <li>Cable cods</li>
+                <li>Printing</li>
+            </ol>
+        </div>
+    )
+}
